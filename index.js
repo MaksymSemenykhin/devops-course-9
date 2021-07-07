7c872d3417d93a3fe07557b9229552be20ac636f
Maksym Semenykhin <MaksymSemenykhin@users.noreply.github.com>
2021-07-07 17:32:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) { ////create a server object:
   const queryObject = url.parse(req.url,true).query;
 
 //   console.log(queryObject);
-//   console.log(queryObject.branch);
+  console.log(queryObject.branch);
 //   console.log(branchParser.splitSentences(queryObject.branch));
   
   res.write('Hello World!'); //write a response to the client
