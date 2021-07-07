a35d28f82abeefc71ee43e2c5e4c625b98002f2d
Maksym Semenykhin <MaksymSemenykhin@users.noreply.github.com>
2021-07-07 17:25:14 +0300

    Add url.parse

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var http = require('http');
+const url = require('url');
 
 http.createServer(function (req, res) { ////create a server object:
 
+  const queryObject = url.parse(req.url,true).query;
+  console.log(queryObject);
+
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-  
+
 }).listen(process.env.NODE_PORT); //the server object listens on port 8080
